25a82b6cfb7cff90b9154d3d4fcbdef74e5b3ea1
Sean Swan <ogpswan@YAHOO.COM>
2019-01-02 11:56:25 -0800

    Javascipt Playground/anime firdt commit

---
+++ main.js
@@ -0,0 +1,64 @@
+var playPause = anime({
+  targets: 'div.feature.box',
+  translateY: [
+  { value: 200, duration: 500 },
+  { value: 0, duration: 800 }
+  ],
+
+  rotate:{
+    value: '1turn',
+    easing: 'easeInOutSine'
+  },
+  // backgroundColor:'#000',  //change divs to black one by one
+  //opacity: '0.3',  // each time nthe box animatres it will turn transparent after
+  delay: function(el, i, l){ return i * 1000},
+  autoplay: false,
+  loop: true,
+});
+
+document.querySelector('#feature-box .play').onclick =
+playPause.play;
+document.querySelector('#feature-box .pause').onclick =
+playPause.pause;
+
+/*anime({
+  targets: 'div.box.blue',
+  translateY: [
+  { value: 200, duration: 500, delay: 1000},
+  { value: 0, duration: 800 }
+  ],
+
+  rotate:{
+    value: '1turn',
+    easing: 'easeInOutSine',
+    delay: 1000,
+  }
+});
+
+anime({
+  targets: 'div.box.green',
+  translateY: [
+  { value: 200, duration: 500, delay: 2000 },
+  { value: 0, duration: 800 }
+  ],
+
+  rotate:{
+    value: '1turn',
+    easing: 'easeInOutSine',
+    delay: 2000
+  }
+});
+
+anime({
+  targets: 'div.box.yellow',
+  translateY: [
+  { value: 200, duration: 500, delay: 3000 },
+  { value: 0, duration: 800 }
+  ],
+
+  rotate:{
+    value: '1turn',
+    easing: 'easeInOutSine',
+    delay: 3000
+  }
+});*/
