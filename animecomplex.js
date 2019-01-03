27fa3aa748ed7cc1a9676aa450549cffff08fd89
Sean Swan <ogpswan@YAHOO.COM>
2019-01-02 19:38:10 -0800

    added personal svg logo linked in polygon trial adusting code

--- animecomplex.js
+++ animecomplex.js
@@ -1,4 +1,4 @@
-var boxShadow = anime({
+var boxShadow = anime2({
   targets: '.box-shadow',
   boxShadow: [
     {value: ['10px -10px 10px 0 currentColor', '10px 10px 20px 0 currentColor']},
@@ -9,50 +9,50 @@ var boxShadow = anime({
   loop: true,
   easing: 'linear'
 });
-var unitLess = anime({
-  
+var unitLess = anime2({
+
   targets: '.unit-less',
   borderRadius: 10,
   loop: true,
   direction: 'alternate',
   easing: 'linear'
 });
-var specifiedUnit = anime({
+var specifiedUnit = anime2({
   targets: '.specified-unit',
   borderRadius: ['0px', '50px'],
   loop: true,
   direction: 'alternate',
   easing: 'linear'
 });
-var filter = anime({
+var filter = anime2({
   targets: '.filter',
   filter: ["blur(20px)", "blur(0px)"],
   loop: true,
   direction: 'alternate',
   easing: 'linear'
 });
-var rgba = anime({
+var rgba = anime2({
   targets: '.rgba',
   backgroundColor: ['rgba(255,100,50,1)', 'rgba(255,100,50,.0)'],
   loop: true,
   direction: 'alternate',
   easing: 'linear'
 });
-var calc = anime({
+var calc = anime2({
   targets: '.calc',
   height: ['calc(25% - 100px)', 'calc(25% - 0px)'],
   loop: true,
   direction: 'alternate',
   easing: 'linear'
 });
-var zeroValue = anime({
+var zeroValue = anime2({
   targets: '.zero-value',
   scale: 0,
   loop: true,
   direction: 'alternate',
   easing: 'linear'
 });
-var roundValue = anime({
+var roundValue = anime2({
   targets: '.round-value',
   translateY: 100,
   loop: true,
@@ -60,7 +60,7 @@ var roundValue = anime({
   direction: 'alternate',
   easing: 'linear'
 });
-var clipPath = anime({
+var clipPath = anime2({
   targets: '.clip-path',
   clipPath: ['circle(0px at center)', 'circle(100px at center)'],
   loop: true,
